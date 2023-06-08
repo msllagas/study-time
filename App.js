aa11e0e9941f1edb965fa5b2e58456cf6c69b8ef
msllagas <mndyllagas@gmail.com>
2023-06-08 12:56:52 +0800

    Changed initial router from Login to StartUp

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./src/screens/Login";
+import StartUp from "./src/screens/StartUp";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="StartUp">
+        <Stack.Screen name="StartUp" component={StartUp} options={{ headerShown: false }}/>
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
