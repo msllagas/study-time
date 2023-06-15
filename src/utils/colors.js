feef7fa5e68bba6b9c94fea9caf67505df06f94c
msllagas <mndyllagas@gmail.com>
2023-06-15 21:54:48 +0800

    Added colors.js utility (Subject to change)

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    beige: '#F9BC60',
+    blueGreen: '#078080',
+    blueGreen1: 'rgba(7,128,128,.3)',
+    skyBlue: '#DFF9F9',
+    redOrange: '#F45D48',
+    lightPink: '#F9EAE8',
+    lightGrey: '#BDBDBD',
+    white: '#FFFFFF'
+}
