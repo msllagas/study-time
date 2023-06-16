cd165cdbb2c6d7e00b8448263d7a5ff349778308
msllagas <mndyllagas@gmail.com>
2023-06-16 13:46:28 +0800

    Added MainPage to the Stack Navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./src/screens/Login";
 import StartUp from "./src/screens/StartUp";
+import MainPage from "./src/screens/MainPage";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="StartUp">
         <Stack.Screen name="StartUp" component={StartUp} options={{ headerShown: false }}/>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Main" component={MainPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
