4ca1b418c3644ae09a5b78ce8bc5850691dc766c
Jessica Faye <jessicafayev026@gmail.com>
2023-06-17 01:32:07 +0800

    edit colors, StartUp
    *added a folders for images
    *added colors in utils
    *edited StartUp
    -added bg
    -added logo
    *added pomodoro.jsx

---
+++ src/screens/PomodoroDashboard.jsx
@@ -0,0 +1,33 @@
+import {
+    View,
+    Text,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    StyleSheet,
+    Pressable,
+    ImageBackground,
+    Image,
+  } from "react-native";
+  import { useNavigation } from "@react-navigation/native";
+  import { Button } from "react-native-paper";
+  import { colors } from "../utils/colors";
+  import logo from "../../assets/imgs/logo3.png";
+
+
+  const PomodoroDashboard = () =>{
+
+    return (
+      <SafeAreaView>
+
+        <View>
+
+        </View>
+      </SafeAreaView>
+    );
+  };
+  export default PomodoroDashboard;
+
+  const styles = StyleSheet.create({
+
+  });
