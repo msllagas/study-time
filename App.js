8478dadf5577ad6ed7a23cbe8cfc96afec45256e
msllagas <mndyllagas@gmail.com>
2023-06-19 15:28:45 +0800

    Added the Drawer navigation to the stack

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
+import 'react-native-gesture-handler';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./src/screens/Login";
 import StartUp from "./src/screens/StartUp";
 import MainPage from "./src/screens/MainPage";
 import Register from "./src/screens/Register";
+import Drawer from './src/screens/navigations/Drawer';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+
+  // For development purposes only. Change in initialRouteName to 'StartUp' for production.
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StartUp">
-        <Stack.Screen name="StartUp" component={StartUp} options={{ headerShown: false }}/>
-        <Stack.Screen name="Login" component={Login}  options={{ headerShown: false }}/>
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
-        <Stack.Screen name="Main" component={MainPage} />
+      <Stack.Navigator initialRouteName="Drawer" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="StartUp" component={StartUp}/>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register}/>
+        <Stack.Screen name="Drawer" component={Drawer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
