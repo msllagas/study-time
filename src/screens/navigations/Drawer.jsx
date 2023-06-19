7696da742bbd62cb4525212dd049cad224c196d4
msllagas <mndyllagas@gmail.com>
2023-06-19 15:30:01 +0800

    Added Drawer Navigation Component

---
+++ src/screens/navigations/Drawer.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { View, Text, BackHandler } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import MainPage from "../MainPage";
+
+const Drawer = () => {
+  const Drawer = createDrawerNavigator();
+  useEffect(() => {
+    const handleExitApp = () => {
+      BackHandler.exitApp(); // Exit the app when back button is pressed
+      return true; // Prevent default back button behavior
+    };
+
+    // Add event listener for the back button
+    BackHandler.addEventListener("hardwareBackPress", handleExitApp);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleExitApp);
+    };
+  }, []);
+  return (
+    <Drawer.Navigator useLegacyImplementation initialRouteName="Main">
+      <Drawer.Screen
+        name="Main"
+        component={MainPage}
+        options={{ headerTitle: "Study Time!" }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default Drawer;
