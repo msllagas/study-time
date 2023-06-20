672f7dab94c25d93b3f42d1a8554c4ef2471fbc4
Jessica Faye <jessicafayev026@gmail.com>
2023-06-20 20:13:56 +0800

    header component
    - header component for dashboards
    - other custom fonts added
    - edit pomodoro & active recall
    - Top bar still in progress atm

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import 'react-native-gesture-handler';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from 'expo-font';
 import Login from "./src/screens/Login";
 import StartUp from "./src/screens/StartUp";
 import MainPage from "./src/screens/MainPage";
 import Register from "./src/screens/Register";
 import Drawer from './src/screens/navigations/Drawer';
+import Pomodoro from './src/screens/Pomodoro';
+import ActiveRecall from './src/screens/ActiveRecall';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [loaded] = useFonts({
+    RockSalt: require('./assets/fonts/RockSalt-Regular.ttf'),
+    AmaticBold: require('./assets/fonts/AmaticSC-Bold.ttf'),
+    AmaticRegular: require('./assets/fonts/AmaticSC-Regular.ttf'),
+});
 
+if (!loaded) {
+    return null;}
   // For development purposes only. Change in initialRouteName to 'StartUp' for production.
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Drawer" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="Pomodoro" screenOptions={{headerShown: false}}>
         <Stack.Screen name="StartUp" component={StartUp}/>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register}/>
         <Stack.Screen name="Drawer" component={Drawer} />
+        <Stack.Screen name="Pomodoro" component={Pomodoro}/>
+        <Stack.Screen name="ActiveRecall" component={ActiveRecall}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/components/Done.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Done = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Done...</Text>
+        </View>
+      );
+}
+
+export default Done
