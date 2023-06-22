879378934c0e82c9c8c65773499bf5d46a9e53e3
msllagas <94226891+msllagas@users.noreply.github.com>
2023-06-22 21:00:40 +0800

    technique utility for study methods

---
+++ src/utils/techniques.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    title: "Pomodoro Method",
+  },
+  {
+    id: 2,
+    title: "Spaced Repetition",
+  },
+  {
+    id: 3,
+    title: "Active Recall",
+  },
+  {
+    id: 4,
+    title: "SQR3 Method",
+  },
+  {
+    id: 5,
+    title: "PQ4R Method",
+  },
+];
