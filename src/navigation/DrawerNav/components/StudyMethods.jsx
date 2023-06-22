605ecb396998a7db5624b895975bae479a495ff2
msllagas <94226891+msllagas@users.noreply.github.com>
2023-06-22 21:06:28 +0800

    added dynamic components based on technique utils

--- src/navigation/DrawerNav/components/StudyMethods.jsx
+++ src/navigation/DrawerNav/components/StudyMethods.jsx
@@ -1,15 +1,51 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Animated, PanResponder, Dimensions } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native";
+import { Button, Card, IconButton, MD3Colors } from "react-native-paper";
+import { colors } from "../../../utils/colors";
+import techniques from "../../../utils/techniques";
 
-import TopicCard from '../../../components/TopicCard';
-
+const StudyMethodsCard = ({ title }) => {
+  const cardWidth = Dimensions.get("window").width * 0.9;
+  return (
+    <View
+      style={{
+        position: "relative",
+        height: 100,
+        width: cardWidth,
+        borderColor: "black",
+        borderWidth: 1,
+        marginTop: 15,
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: colors.redOrange,
+        borderBottomWidth: 4,
+        elevation: 15,
+        shadowColor: colors.blueGreen,
+      }}
+    >
+      <IconButton
+        icon="help-circle-outline"
+        iconColor="white"
+        size={25}
+        style={{ position: "absolute", top: 0, right: 0 }}
+        onPress={() => console.log("Pressed")}
+      />
+      <View>
+        <Text style={{ color: colors.blueGreen, fontSize: 26 }}>{title}</Text>
+      </View>
+    </View>
+  );
+};
 
 const StudyMethods = () => {
   return (
-    <View style={{flex: 1}}>
-      <TopicCard tag="pomodoro" topic="Test 1"/>
-    </View>
-  )
-}
+    <ScrollView contentContainerStyle={{ alignItems: "center", paddingBottom: 20 }} showsVerticalScrollIndicator={false}>
+      {techniques.map((technique) => (
+        <StudyMethodsCard key={technique.id} title={technique.title} />
+      ))}
+    </ScrollView>
+  );
+};
 
 export default StudyMethods;
