60d3683a4fcd1fef5583ea5e2e7e794780e1b5f1
msllagas <mndyllagas@gmail.com>
2023-06-23 13:40:34 +0800

    make the component slidable

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,47 +1,165 @@
-import { View, Text, StyleSheet, Dimensions, PanResponder, Animated } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import React from "react";
-import { colors } from "../utils/colors";
+import React, { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  PanResponder,
+  Animated,
+} from "react-native";
+import { IconButton } from "react-native-paper";
 
-const TopicCard = ({ tag, topic="Topic Here" }) => {
+const TopicCard = ({ tag, topic = "Topic Here" }) => {
+  const slideAnimation = useRef(new Animated.Value(0)).current;
+  const buttonOpacity = useRef(new Animated.Value(0)).current;
+  const buttonScale = useRef(new Animated.Value(0)).current;
+  const [slidePosition, setSlidePosition] = useState(0);
+  const [isSliding, setIsSliding] = useState(false);
 
-  const cardWidth = Dimensions.get('window').width * 0.9;
+  const buttonWidth = 60;
+  const sensitivity = 50000; // Increase this value for higher sensitivity
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, gestureState) => {
+        if (isSliding) {
+          slideAnimation.setValue(gestureState.dx / sensitivity); // Adjust sensitivity by dividing dx value
+        }
+      },
+      onPanResponderGrant: () => {
+        setIsSliding(true);
+      },
+      onPanResponderRelease: (_, gestureState) => {
+        setIsSliding(false);
+        if (slidePosition === 0 && gestureState.dx < -50) {
+          slideToPosition(-buttonWidth);
+        } else if (slidePosition === -buttonWidth && gestureState.dx > 50) {
+          slideToPosition(0);
+        } else {
+          slideToPosition(slidePosition);
+        }
+      },
+    })
+  ).current;
+
+  const slideToPosition = (position) => {
+    Animated.parallel([
+      Animated.spring(slideAnimation, {
+        toValue: position,
+        useNativeDriver: true,
+        bounciness: 8,
+        speed: 12,
+      }),
+      Animated.timing(buttonOpacity, {
+        toValue: position === -buttonWidth ? 1 : 0,
+        duration: position === -buttonWidth ? 200 : 100,
+        useNativeDriver: true,
+      }),
+      Animated.spring(buttonScale, {
+        toValue: position === -buttonWidth ? 1 : 0,
+        useNativeDriver: true,
+        bounciness: 8,
+        speed: 12,
+      }),
+    ]).start(() => {
+      setSlidePosition(position);
+    });
+  };
+
+  const buttonOpacityAnimation = buttonOpacity.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+    extrapolate: "clamp",
+  });
+
+  const buttonScaleAnimation = buttonScale.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+    extrapolate: "clamp",
+  });
+
+  const textContainerTranslateX = slideAnimation.interpolate({
+    inputRange: [-buttonWidth, 0],
+    outputRange: [-buttonWidth, 0],
+    extrapolate: "clamp",
+  });
+
+  const cardWidth = Dimensions.get("window").width * 0.9;
+  const buttonSize = cardWidth * 0.1;
   let backgroundColor;
   let borderColor;
   let method;
 
   if (tag === "pomodoro") {
-    backgroundColor = colors.violet;
-    borderColor = colors.violet;
-    method = 'Pomodoro Technique'
+    backgroundColor = "#8e44ad";
+    borderColor = "#8e44ad";
+    method = "Pomodoro Technique";
   } else if (tag === "spaced repetition") {
-    backgroundColor = colors.beige;
-    borderColor = colors.beige;
-    method = 'Spaced Repetition'
+    backgroundColor = "#f39c12";
+    borderColor = "#f39c12";
+    method = "Spaced Repetition";
   } else if (tag === "active recall") {
-    backgroundColor = colors.green;
-    borderColor = colors.green;
-    method = 'Active Recall'
+    backgroundColor = "#27ae60";
+    borderColor = "#27ae60";
+    method = "Active Recall";
   } else if (tag === "pq4r") {
-    backgroundColor = colors.skyBlue;
-    borderColor = colors.skyBlue;
-    method = 'PQ4R Method'
+    backgroundColor = "#2980b9";
+    borderColor = "#2980b9";
+    method = "PQ4R Method";
   } else if (tag === "sq3r") {
-    backgroundColor = colors.purple;
-    borderColor = colors.purple;
-    method = 'SQ3R Method'
+    backgroundColor = "#9b59b6";
+    borderColor = "#9b59b6";
+    method = "SQ3R Method";
   }
+
   return (
     <View style={styles.container}>
-      <Text style={[styles.textContainer, { backgroundColor, borderColor }]}>
-        <Text style={[styles.text]}>{method}</Text>
-      </Text>
-      <View style={styles.cardContainer}>
-        <View style={[styles.card, { borderColor, width: cardWidth }]}>
-          <Text style={styles.topicText}>{topic}</Text>
-          <Ionicons name="trash-bin-outline" size={20} color="red" />
-        </View>
-      </View>
+      <Animated.View
+        style={[
+          styles.textContainer,
+          {
+            backgroundColor: backgroundColor,
+            borderColor: borderColor,
+            transform: [{ translateX: textContainerTranslateX }],
+          },
+        ]}
+      >
+        <Text style={styles.text}>{method}</Text>
+      </Animated.View>
+      <Animated.View
+        style={[
+          styles.card,
+          {
+            transform: [{ translateX: slideAnimation }],
+            width: cardWidth,
+            borderColor: borderColor,
+          },
+        ]}
+        {...panResponder.panHandlers}
+      >
+        <Text style={styles.topicText}>{topic}</Text>
+      </Animated.View>
+      <Animated.View
+        style={[
+          styles.buttonContainer,
+          {
+            opacity: buttonOpacityAnimation,
+            transform: [{ scale: buttonScaleAnimation }],
+          },
+        ]}
+      >
+        <IconButton
+          mode="contained-tonal"
+          icon="delete-outline"
+          rippleColor="white"
+          containerColor="red"
+          iconColor="white"
+          size={buttonSize}
+          onPress={() => console.log("Pressed")}
+        />
+      </Animated.View>
     </View>
   );
 };
@@ -54,8 +172,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginVertical: 5,
-    
-    
   },
   card: {
     borderWidth: 1,
@@ -66,10 +182,6 @@ const styles = StyleSheet.create({
     position: "relative",
     flexDirection: "row",
     minHeight: 60,
-  },
-  cardContainer: {
-    alignSelf: "center",
-    marginVertical: 5,
   },
   textContainer: {
     position: "absolute",
@@ -84,11 +196,19 @@ const styles = StyleSheet.create({
     fontSize: 12,
     textAlign: "center",
     color: "white",
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   topicText: {
     textAlign: "center",
-    fontWeight: 'bold',
-    fontSize: 16
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  buttonContainer: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    paddingRight: 10,
   },
 });
