760d03e96a2bf48ff9ddbfeba455fe9efe1b3437
msllagas <mndyllagas@gmail.com>
2023-06-23 13:39:37 +0800

    added description field for each technique

--- src/utils/techniques.js
+++ src/utils/techniques.js
@@ -2,21 +2,30 @@ export default [
   {
     id: 1,
     title: "Pomodoro Method",
+    description: "Please write here the description of this method.",
   },
   {
     id: 2,
     title: "Spaced Repetition",
+
+    description: "Please write here the description of this method.",
   },
   {
     id: 3,
     title: "Active Recall",
+
+    description: "Please write here the description of this method.",
   },
   {
     id: 4,
     title: "SQR3 Method",
+
+    description: "Please write here the description of this method.",
   },
   {
     id: 5,
     title: "PQ4R Method",
+
+    description: "Please write here the description of this method.",
   },
 ];
