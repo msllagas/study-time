9a37d0637ed736e8e2acf75eb1dab4203e2b542b
msllagas <94226891+msllagas@users.noreply.github.com>
2023-06-26 18:58:40 +0800

    make Done and Ongoing component more dynamic

--- src/navigation/TopTabNav/TopBar.jsx
+++ src/navigation/TopTabNav/TopBar.jsx
@@ -1,38 +1,56 @@
-import * as React from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
-import { colors } from '../../utils/colors';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Done from './components/Done';
-import Ongoing from './components/Ongoing';
+import * as React from "react";
+import { colors } from "../../utils/colors";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
+const TopBar = ({ Done, Ongoing, tag }) => {
+  const Tab = createMaterialTopTabNavigator();
+  return (
+    <Tab.Navigator
+      initialRouteName="Done"
+      screenOptions={{
+        activeTintColor: colors.violet,
+        labelStyle: { fontSize: 14 },
+        style: { backgroundColor: "white" },
+      }}
+    >
+      <Tab.Screen name="Done" options={{ tabBarLabel: "Done" }}>
+        {() => <Done tag={tag} />}
+      </Tab.Screen>
+      <Tab.Screen name="On-going" options={{ tabBarLabel: "On-going" }}>
+        {() => <Ongoing tag={tag} />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
-function StatusTabs() {
-    const Tab = createMaterialTopTabNavigator();
-    return(
-        <Tab.Navigator
-        initialRouteName='Done'
-        screenOptions={{
-            activeTintColor: colors.violet,
-            labelStyle: {fontSize: 14},
-            style: {backgroundColor: 'white'}
-        }}>
-            <Tab.Screen
-                name='Done'
-                component={Done}
-                options={{tabBarLabel:'Done'}}
-            />
-            <Tab.Screen
-                name='On-going'
-                component={Ongoing}
-                options={{tabBarLabel:'On-going'}}
-            />
-        </Tab.Navigator>
-    )
-}
+export default TopBar;
 
-export default function TopBar(){
-    return(
-            <StatusTabs/>
-    )
-}
+// function StatusTabs() {
+//     const Tab = createMaterialTopTabNavigator();
+//     return(
+//         <Tab.Navigator
+//         initialRouteName='Done'
+//         screenOptions={{
+//             activeTintColor: colors.violet,
+//             labelStyle: {fontSize: 14},
+//             style: {backgroundColor: 'white'}
+//         }}>
+//             <Tab.Screen
+//                 name='Done'
+//                 component={Done}
+//                 options={{tabBarLabel:'Done'}}
+//             />
+//             <Tab.Screen
+//                 name='On-going'
+//                 component={Ongoing}
+//                 options={{tabBarLabel:'On-going'}}
+//             />
+//         </Tab.Navigator>
+//     )
+// }
+
+// export default function TopBar(){
+//     return(
+//             <StatusTabs/>
+//     )
+// }
