df158facd695832121af0fd9273f646fa22e4f59
msllagas <mndyllagas@gmail.com>
2023-06-28 13:18:07 +0800

    fixed stylings, added loading indicator

--- src/components/Done.jsx
+++ src/components/Done.jsx
@@ -1,22 +1,19 @@
-import {ScrollView } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 import { FIRESTORE_DB } from "../../firebaseConfig";
 import TopicCard from "./TopicCard";
 
-const Done = ({ tag}) => {
+const Done = ({ tag }) => {
   const [doneTopics, setDoneTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchDoneTopics = async () => {
       try {
         let doneTopicsQuery;
-  
+
         if (tag) {
           doneTopicsQuery = query(
             collection(FIRESTORE_DB, "topics"),
@@ -29,34 +26,63 @@ const Done = ({ tag}) => {
             where("isDone", "==", true)
           );
         }
-  
+
         const doneTopicsSnapshot = await getDocs(doneTopicsQuery);
         const doneTopicsData = doneTopicsSnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
+        setIsLoading(false);
         setDoneTopics(doneTopicsData);
       } catch (error) {
         console.log("Error fetching done topics:", error);
       }
     };
-  
+
     fetchDoneTopics();
   }, [tag, doneTopics]);
-  
+
   return (
-    <ScrollView
-      contentContainerStyle={{
-        justifyContent: "center",
-        marginTop: 15,
-        paddingBottom: 50,
-      }}
-    >
-      {doneTopics.map((topic) => (
-        <TopicCard key={topic.id} tag={topic.tag} topic={topic.title} />
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      {isLoading && (
+        <View style={styles.indicatorContainer}>
+          <ActivityIndicator
+            animating={true}
+            color={MD2Colors.red800}
+            size="large"
+            style={{ alignSelf: "center" }}
+          />
+        </View>
+      )}
+
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        {doneTopics.map((topic) => (
+          <TopicCard key={topic.id} tag={topic.tag} topic={topic.title} />
+        ))}
+        {/* <TopicCard tag="pomodoro" topic="Sample awdawdawda wd awd awd awda daw dawd awdawdawdawd awdawd awd awd awd awd "/> */}
+      </ScrollView>
+    </View>
   );
 };
 
 export default Done;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  indicatorContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scrollView: {
+    justifyContent: "center",
+    marginTop: 15,
+    paddingBottom: 50,
+  },
+});
--- src/components/Ongoing.jsx
+++ src/components/Ongoing.jsx
@@ -1,11 +1,13 @@
-import { ScrollView } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 import { FIRESTORE_DB } from "../../firebaseConfig";
 import TopicCard from "./TopicCard";
 
-const Ongoing = ({tag}) => {
+const Ongoing = ({ tag }) => {
   const [ongoingTopics, setOngoingTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchDoneTopics = async () => {
@@ -30,6 +32,7 @@ const Ongoing = ({tag}) => {
           id: doc.id,
           ...doc.data(),
         }));
+        setIsLoading(false);
         setOngoingTopics(doneTopicsData);
       } catch (error) {
         console.log("Error fetching done topics:", error);
@@ -40,18 +43,49 @@ const Ongoing = ({tag}) => {
   }, [tag, ongoingTopics]);
 
   return (
-    <ScrollView
-      contentContainerStyle={{
-        justifyContent: "center",
-        marginTop: 15,
-        paddingBottom: 50,
-      }}
-    >
-      {ongoingTopics.map((topic) => (
-        <TopicCard key={topic.id} tag={topic.tag} topic={topic.title} />
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      {isLoading && (
+        <View
+          style={styles.indicatorContainer}
+        >
+          <ActivityIndicator
+            animating={true}
+            color={MD2Colors.red800}
+            size="large"
+          />
+        </View>
+      )}
+
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+      >
+        {ongoingTopics.map((topic) => (
+          <TopicCard key={topic.id} tag={topic.tag} topic={topic.title} />
+        ))}
+        {/* <TopicCard tag="pomodoro" topic="Sample awdawdawda wd awd awd awda daw dawd awdawdawdawd awdawd awd awd awd awd "/> */}
+      </ScrollView>
+    </View>
   );
 };
 
 export default Ongoing;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  indicatorContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scrollView: {
+    justifyContent: "center",
+    marginTop: 15,
+    paddingBottom: 50,
+  }
+});
