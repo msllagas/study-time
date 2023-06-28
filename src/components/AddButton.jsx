71081f1ddb2ac5b97ca502c41d553b2a21455116
msllagas <mndyllagas@gmail.com>
2023-06-28 13:16:01 +0800

    fix issues in stylings, removed view wrapper

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -13,41 +13,28 @@ import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
   usage
   <AddButton onPressAdd={()=>console.log("pressed")}
 */
-const AddButton = ({onPressAdd}) => {
+const AddButton = ({ onPressAdd }) => {
   return (
-    <View style={styles.fabView}>
     <FAB
       icon="plus"
       style={styles.fab}
       color={colors.blueGreen}
       rippleColor={colors.blueGreen2}
       onPress={onPressAdd}
-      
+      customSize={60}
     />
-    </View>
   );
 };
 
 const styles = StyleSheet.create({
   fab: {
-    // position: "fixed",
-    // bottom: "50px",
-    // right: "20px",
-    // width: "55px",
-    // borderRadius:30
-    position: 'absolute',
-      margin: 16,
-      right: 0,
-      //bottom: 0,
-      borderRadius:30,
-      // backgroundColor: colors.blueGreen1,
-      backgroundColor: colors.white,
-  },
-  fabView:{
-    flex:1,
-    justifyContent: 'flex-end',
-    bottom: 50
-    // marginBottom: 50
+    position: "absolute",
+    margin: 16,
+    bottom: 10,
+    right: 10,
+    borderRadius: 30,
+
+    backgroundColor: colors.white,
   },
 });
 
