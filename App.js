0aa717e7dbd696e20a9296868c622194b6ee37cd
Trianglebucks <blazerx1234@gmail.com>
2023-06-30 00:18:01 +0800

    spaced rep date picker

--- App.js
+++ App.js
@@ -1,53 +1,73 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 import Login from "./src/screens/Login";
 import StartUp from "./src/screens/StartUp";
 import MainPage from "./src/screens/MainPage";
 import Register from "./src/screens/Register";
-import Drawer from './src/navigation/DrawerNav/Drawer';
-import ActiveRecall from './src/navigation/DrawerNav/components/ActiveRecall/ActiveRecall';
-import ActiveRecallAdd from './src/navigation/DrawerNav/components/ActiveRecall/ActiveRecallAdd';
-import ActiveRecallQuiz from './src/navigation/DrawerNav/components/ActiveRecall/ActiveRecallQuiz';
-import ActiveRecallDone from './src/navigation/DrawerNav/components/ActiveRecall/ActiveRecallDone';
-import Pomodoro from './src/navigation/DrawerNav/components/Pomodoro/Pomodoro';
-import PomodoroAdd from './src/navigation/DrawerNav/components/Pomodoro/PomodoroAdd';
-import PomodoroTimer from './src/navigation/DrawerNav/components/Pomodoro/PomodoroTimer';
-import PomodoroDone from './src/navigation/DrawerNav/components/Pomodoro/PomodoroDone';
+import Drawer from "./src/navigation/DrawerNav/Drawer";
+import ActiveRecall from "./src/navigation/DrawerNav/components/ActiveRecall/ActiveRecall";
+import ActiveRecallAdd from "./src/navigation/DrawerNav/components/ActiveRecall/ActiveRecallAdd";
+import ActiveRecallQuiz from "./src/navigation/DrawerNav/components/ActiveRecall/ActiveRecallQuiz";
+import ActiveRecallDone from "./src/navigation/DrawerNav/components/ActiveRecall/ActiveRecallDone";
+import Pomodoro from "./src/navigation/DrawerNav/components/Pomodoro/Pomodoro";
+import PomodoroAdd from "./src/navigation/DrawerNav/components/Pomodoro/PomodoroAdd";
+import PomodoroTimer from "./src/navigation/DrawerNav/components/Pomodoro/PomodoroTimer";
+import PomodoroDone from "./src/navigation/DrawerNav/components/Pomodoro/PomodoroDone";
+import SpacedRepitition from "./src/navigation/DrawerNav/components/SpacedRepitition/SpacedRepitition";
+import SpacedRepititionAdd from "./src/navigation/DrawerNav/components/SpacedRepitition/SpacedRepititionAdd";
+import SpacedRepititionDateStart from "./src/navigation/DrawerNav/components/SpacedRepitition/SpacedRepititionDateStart";
+import SpacedRepititionDateEnd from "./src/navigation/DrawerNav/components/SpacedRepitition/SpacedRepititionDateEnd";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   const [loaded] = useFonts({
-    RockSalt: require('./assets/fonts/RockSalt-Regular.ttf'),
-    AmaticBold: require('./assets/fonts/AmaticSC-Bold.ttf'),
-    AmaticRegular: require('./assets/fonts/AmaticSC-Regular.ttf'),
-    AlumniSansThin: require('./assets/fonts/AlumniSans-Thin.ttf'),
-    AlumniSansRegular: require('./assets/fonts/AlumniSans-Regular.ttf'),
-    FuzzyBubblesBold: require('./assets/fonts/FuzzyBubbles-Bold.ttf'),
-    FuzzyBubblesRegular: require('./assets/fonts/FuzzyBubbles-Regular.ttf'),
-    Inter: require ('./assets/fonts/Inter-Regular.ttf')
+    RockSalt: require("./assets/fonts/RockSalt-Regular.ttf"),
+    AmaticBold: require("./assets/fonts/AmaticSC-Bold.ttf"),
+    AmaticRegular: require("./assets/fonts/AmaticSC-Regular.ttf"),
+    AlumniSansThin: require("./assets/fonts/AlumniSans-Thin.ttf"),
+    AlumniSansRegular: require("./assets/fonts/AlumniSans-Regular.ttf"),
+    FuzzyBubblesBold: require("./assets/fonts/FuzzyBubbles-Bold.ttf"),
+    FuzzyBubblesRegular: require("./assets/fonts/FuzzyBubbles-Regular.ttf"),
+    Inter: require("./assets/fonts/Inter-Regular.ttf"),
+  });
 
-});
-
-if (!loaded) {
-    return null;}
+  if (!loaded) {
+    return null;
+  }
   // For development purposes only. Change in initialRouteName to 'StartUp' for production.
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Drawer" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="StartUp" component={StartUp}/>
+      <Stack.Navigator
+        initialRouteName="Drawer"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="StartUp" component={StartUp} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register}/>
+        <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Drawer" component={Drawer} />
-        <Stack.Screen name="ActiveRecall" component={ActiveRecall}/>
-        <Stack.Screen name="ActiveRecallAdd" component={ActiveRecallAdd}/>
-        <Stack.Screen name="ActiveRecallQuiz" component={ActiveRecallQuiz}/>
-        <Stack.Screen name="ActiveRecallDone" component={ActiveRecallDone}/>
-        <Stack.Screen name="Pomodoro" component={Pomodoro}/>
-        <Stack.Screen name="PomodoroAdd" component={PomodoroAdd}/>
-        <Stack.Screen name="PomodoroTimer" component={PomodoroTimer}/>
-        <Stack.Screen name="PomodoroDone" component={PomodoroDone}/>
+        <Stack.Screen name="ActiveRecall" component={ActiveRecall} />
+        <Stack.Screen name="ActiveRecallAdd" component={ActiveRecallAdd} />
+        <Stack.Screen name="ActiveRecallQuiz" component={ActiveRecallQuiz} />
+        <Stack.Screen name="ActiveRecallDone" component={ActiveRecallDone} />
+        <Stack.Screen name="Pomodoro" component={Pomodoro} />
+        <Stack.Screen name="PomodoroAdd" component={PomodoroAdd} />
+        <Stack.Screen name="PomodoroTimer" component={PomodoroTimer} />
+        <Stack.Screen name="PomodoroDone" component={PomodoroDone} />
+        <Stack.Screen name="SpacedRepitition" component={SpacedRepitition} />
+        <Stack.Screen
+          name="SpacedRepititionAdd"
+          component={SpacedRepititionAdd}
+        />
+        <Stack.Screen
+          name="SpacedRepititionDateStart"
+          component={SpacedRepititionDateStart}
+        />
+        <Stack.Screen
+          name="SpacedRepititionDateEnd"
+          component={SpacedRepititionDateEnd}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/navigation/DrawerNav/components/SpacedRepitition/SpacedRepititionAdd.jsx
+++ src/navigation/DrawerNav/components/SpacedRepitition/SpacedRepititionAdd.jsx
@@ -1,7 +1,194 @@
+import {
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Pressable,
+  Image,
+  ScrollView,
+} from "react-native";
 import React from "react";
+import {
+  Button,
+  Appbar,
+  IconButton,
+  TextInput,
+  Modal,
+  Portal,
+  Text,
+  PaperProvider,
+} from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { colors } from "../../../../utils/colors";
+import Constants from "expo-constants";
+import { color } from "react-native-reanimated";
 
-const SpacedRepititionAdd = () => {
-  return <div>SpacedRepititionAdd</div>;
+const SpacedRepititionAdd = ({ navigation }) => {
+  const [topicName, setTopicName] = React.useState("");
+  const [startTime, setStartTime] = React.useState("0");
+
+  const [visible, setVisible] = React.useState(false);
+  const _goBack = () => navigation.goBack();
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {
+    backgroundColor: "white",
+    padding: 20,
+    width: "80%",
+    alignSelf: "center",
+    borderRadius: 8,
+  };
+  return (
+    <SafeAreaView style={styles.viewContainer}>
+      <ScrollView style={{ paddingBottom: 20 }}>
+        <PaperProvider>
+          <IconButton
+            icon="arrow-left"
+            style={styles.backButton}
+            size={40}
+            onPress={_goBack}
+          />
+
+          <IconButton
+            icon="help-circle-outline"
+            iconColor={colors.redOrange}
+            size={25}
+            style={{ position: "absolute", top: 20, right: 0 }}
+            onPress={showModal}
+          />
+
+          <Image
+            style={styles.imageContainer}
+            source={require("../../../../../assets/imgs/logo2.png")}
+            resizeMode="contain"
+          />
+
+          <Text
+            style={{
+              fontFamily: "FuzzyBubblesBold",
+              fontSize: 18,
+              textAlign: "center",
+            }}
+          >
+            Study with Spaced Repitition Technique
+          </Text>
+
+          <Text
+            style={{
+              fontFamily: "AmaticBold",
+              fontSize: 48,
+              textAlign: "center",
+              color: colors.beige,
+              marginTop: 20,
+            }}
+          >
+            Topic Name
+          </Text>
+
+          <TextInput
+            label="Topic"
+            value={topicName}
+            onChangeText={(topicName) => setTopicName(topicName)}
+            style={{
+              width: "70%",
+              alignSelf: "center",
+              marginBottom: 30,
+              backgroundColor: colors.lighterYellow,
+            }}
+            underlineColor={colors.lighterYellow}
+            activeUnderlineColor={colors.beige}
+          />
+
+          <Text
+            style={{
+              fontFamily: "AmaticBold",
+              fontSize: 48,
+              textAlign: "center",
+              color: colors.beige,
+              marginTop: 20,
+            }}
+          >
+            Session Interval
+          </Text>
+
+          <TextInput
+            value={startTime}
+            onChangeText={(startTime) => setStartTime(startTime)}
+            style={styles.setupInput}
+            mode="outlined"
+            outlineColor={colors.lighterYellow}
+            activeOutlineColor={colors.beige}
+          />
+
+          <Button
+            mode="contained"
+            onPress={() => navigation.navigate("SpacedRepititionDateStart")}
+            style={{
+              borderRadius: 8,
+              width: "70%",
+              alignSelf: "center",
+              marginVertical: 30,
+              backgroundColor: colors.beige,
+            }}
+          >
+            Set-up Schedule
+          </Button>
+
+          <Portal>
+            <Modal
+              visible={visible}
+              onDismiss={hideModal}
+              contentContainerStyle={containerStyle}
+            >
+              <Text>Example Modal. Click outside this area to dismiss.</Text>
+            </Modal>
+          </Portal>
+        </PaperProvider>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: colors.white,
+  },
+  backButton: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+  },
+  imageContainer: {
+    alignSelf: "center",
+    width: "50px",
+    marginTop: 20,
+  },
+  setupContainer: {
+    width: "70%",
+    alignSelf: "center",
+    marginVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  largeText: {
+    fontFamily: "AmaticBold",
+    fontSize: 32,
+    color: colors.beige,
+  },
+  smallText: {
+    fontFamily: "AmaticRegular",
+    fontSize: 16,
+    color: colors.beige,
+  },
+  setupInput: {
+    width: "30%",
+    alignSelf: "center",
+    backgroundColor: colors.lighterYellow,
+    textAlign: "center",
+  },
+});
 
 export default SpacedRepititionAdd;
