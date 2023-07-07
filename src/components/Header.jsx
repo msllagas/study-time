07be3743128f83bcf135327ab03932f98ad15bce
msllagas <mndyllagas@gmail.com>
2023-07-07 23:38:31 +0800

    adjust stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,12 +20,12 @@ Import first
 
 const Header = ({title, onPressBackArrow}) =>{ 
     return (
-      <SafeAreaView>       
+   
         <Appbar.Header style={styles.appbarHeader} mode="center-aligned">
           <Appbar.BackAction onPress= {onPressBackArrow} color={colors.redOrange}/>
           <Appbar.Content title={title} color={colors.redOrange} titleStyle={styles.appbarTitle}/>
         </Appbar.Header>
-      </SafeAreaView>
+
     );
   };
   export default Header;
@@ -33,14 +33,12 @@ const Header = ({title, onPressBackArrow}) =>{
   const styles = StyleSheet.create({
     appbarHeader:{
       backgroundColor:colors.grayBlue, 
-      flexDirection:'row',
-      justifyContent:'space-between',
-      alignItems: 'center'
     },
     appbarTitle:{
       fontFamily:'RockSalt',
-      fontSize: 16,
+      fontSize: 20,
       paddingVertical: 10,
-      textAlignVertical:'center'
+      alignSelf: 'center',
+      marginTop: 20
     },
   });
